300873278f5ca5956d68d91447892d3567b9424a
Anas LARIBI <anas.laribi95@gmail.com>
2025-02-04 05:06:10 -0500

    Added Pages and Layout + Routing

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -1,26 +1,29 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListing from "./components/JobListing";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 const App = () => {
   return (
     <>
-      <Navbar />
-
-      <Hero />
-
-      <HomeCards />
-
-      <JobListing />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
-          View All Jobs
-        </a>
-      </section>
+      <RouterProvider router={router} />
     </>
   );
 };
---
+++ react-jobs/src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import JobListing from "../components/JobListing";
+
+const jobsPage = () => {
+  return (
+    <section className="bg-blue-50 px-4 py-6">
+      <JobListing />
+    </section>
+  );
+};
+
+export default jobsPage;
