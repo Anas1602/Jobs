a1e0d680aa1e5b2d93b1cea2495a343f9abe7172
Anas LARIBI <anas.laribi95@gmail.com>
2025-02-05 04:58:31 -0500

    server added

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -9,12 +9,18 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFound from "./pages/NotFound";
+import JobPageSingular, { jobLoader } from "./pages/JobPageSingular";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route
+        path="/jobs/:id"
+        element={<JobPageSingular />}
+        loader={jobLoader}
+      />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
--- react-jobs/src/components/JobListing.jsx
+++ react-jobs/src/components/JobListing.jsx
@@ -1,8 +1,28 @@
-import jobs from "../jobs.json";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
 import JobListingSingular from "./JobListingSingular";
+import Spinner from "./Spinner";
 
 const JobListing = ({ isHome = false }) => {
-  const JobListing = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const ftechJobs = async () => {
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error Fetching Data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    ftechJobs();
+  }, []);
   return (
     <>
       <section className="bg-blue-50 px-4 py-10">
@@ -10,11 +30,16 @@ const JobListing = ({ isHome = false }) => {
           <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
             {isHome ? "Recent Jobs" : "Browse Jobs"}
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {JobListing.map((job) => (
-              <JobListingSingular key={job.id} job={job} />
-            ))}
-          </div>
+
+          {loading ? (
+            <Spinner loading={loading} />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((job) => (
+                <JobListingSingular key={job.id} job={job} />
+              ))}
+            </div>
+          )}
         </div>
       </section>
     </>
